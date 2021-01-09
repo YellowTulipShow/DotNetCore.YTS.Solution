b7edfd0789f732ec07b303e99243aa8c13a665da
YellowTulipShow <main@yellowtulipshow.site>
2021-01-09 14:46:52 +0800

    YTS.TextGame

--- README.md
+++ README.md
@@ -113,6 +113,8 @@ dotnet nuget push <PackagesPath> --api-key <ApiKey> --source https://api.nuget.o
 * [.NET Core 轻量级模板引擎 Mustachio](https://cloud.tencent.com/developer/article/1547299)
 * [C#控制台程序入口函数 Main(string[] args) 参数详解](https://www.cnblogs.com/SavionZhang/p/6526637.html)
 * [.net core/.net 使用 CommandLineParser 来标准化地解析命令行](https://blog.csdn.net/weixin_30950607/article/details/98694618)
+* [使用Visual Studio Code开发.NET Core看这篇就够了](https://www.cnblogs.com/yilezhu/p/9926078.html)
+* [使用 System.CommandLine 分析命令行](https://docs.microsoft.com/zh-cn/archive/msdn-magazine/2019/march/net-parse-the-command-line-with-system-commandline)
 
 ### FreeSql 学习
 
---
+++ YTS.TextGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace YTS.TextGame
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('p', "path", Required = true, HelpText = "文件/夹路径地址")]
+            public string Path { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed(Run);
+        }
+
+        private static void Run(Options option)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(string.Format("Path: {0}", new string[] { option.Path }));
+        }
+    }
+}
