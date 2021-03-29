bf3be4a3551d74537a0cf343367a4bb0e29be13e
YellowTulipShow <main@yellowtulipshow.site>
2021-03-29 09:53:59 +0800

    二进制字符串与常规字符串互转

--- YTS.Tools/ConvertTool.cs
+++ YTS.Tools/ConvertTool.cs
@@ -1113,5 +1113,33 @@ namespace YTS.Tools
             }
             return list[index];
         }
+
+        /// <summary>
+        /// 将字符串转成二进制
+        /// </summary>
+        public static string ToBinarytring(string s)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(s);
+            StringBuilder result = new StringBuilder(data.Length * 8);
+            foreach (byte b in data)
+            {
+                result.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+            }
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// 将二进制转成字符串
+        /// </summary>
+        public static string BinarytringSolve(string s)
+        {
+            CaptureCollection cs = Regex.Match(s, @"([01]{8})+").Groups[1].Captures;
+            byte[] data = new byte[cs.Count];
+            for (int i = 0; i < cs.Count; i++)
+            {
+                data[i] = Convert.ToByte(cs[i].Value, 2);
+            }
+            return Encoding.Unicode.GetString(data, 0, data.Length);
+        }
     }
 }
