2f5a11281c08b465ed5df1d3bbf039eb1eadf3a5
YellowTulipShow <main@yellowtulipshow.site>
2022-01-29 12:26:09 +0800

    更新

--- YTS.Logic/Log/BasicJSONConsolePrintLog.cs
+++ YTS.Logic/Log/BasicJSONConsolePrintLog.cs
@@ -11,18 +11,42 @@ namespace YTS.Logic.Log
     public class BasicJSONConsolePrintLog : ILog
     {
         private const string null_message = "<null_message>";
+
+        /// <summary>
+        /// 常规信息日志写入
+        /// </summary>
+        /// <param name="message">消息内容</param>
+        /// <param name="args">参数队列</param>
         public void Info(string message, params IDictionary<string, object>[] args)
         {
             PrintLines($"[Info] {message ?? null_message}, arg: {JsonConvert.SerializeObject(args)}");
         }
+
+        /// <summary>
+        /// 错误信息日志写入
+        /// </summary>
+        /// <param name="message">消息内容</param>
+        /// <param name="args">参数队列</param>
         public void Error(string message, params IDictionary<string, object>[] args)
         {
             PrintLines($"[Error] {message ?? null_message}, arg: {JsonConvert.SerializeObject(args)}");
         }
+
+        /// <summary>
+        /// 错误信息和异常日志写入
+        /// </summary>
+        /// <param name="message">消息内容</param>
+        /// <param name="ex">异常</param>
+        /// <param name="args">参数队列</param>
         public void Error(string message, Exception ex, params IDictionary<string, object>[] args)
         {
             PrintLines($"[ErrorException] {message ?? null_message}, Exception: {ex.Message + ex.StackTrace} arg: {JsonConvert.SerializeObject(args)}");
         }
+
+        /// <summary>
+        /// 打印输出多行内容
+        /// </summary>
+        /// <param name="msglist">需要打印的多行消息列表</param>
         protected virtual void PrintLines(params string[] msglist)
         {
             foreach (string str in msglist)
--- YTS.Logic/Log/ConnectLog.cs
+++ YTS.Logic/Log/ConnectLog.cs
@@ -24,6 +24,11 @@ namespace YTS.Logic.Log
             }
         }
 
+        /// <summary>
+        /// 常规信息日志写入
+        /// </summary>
+        /// <param name="message">消息内容</param>
+        /// <param name="args">参数队列</param>
         public void Info(string message, params IDictionary<string, object>[] args)
         {
             foreach (var item in logDict.Values)
@@ -31,6 +36,12 @@ namespace YTS.Logic.Log
                 item.Error(message, args);
             }
         }
+
+        /// <summary>
+        /// 错误信息日志写入
+        /// </summary>
+        /// <param name="message">消息内容</param>
+        /// <param name="args">参数队列</param>
         public void Error(string message, params IDictionary<string, object>[] args)
         {
             foreach (var item in logDict.Values)
@@ -38,6 +49,13 @@ namespace YTS.Logic.Log
                 item.Error(message, args);
             }
         }
+
+        /// <summary>
+        /// 错误信息和异常日志写入
+        /// </summary>
+        /// <param name="message">消息内容</param>
+        /// <param name="ex">异常</param>
+        /// <param name="args">参数队列</param>
         public void Error(string message, Exception ex, params IDictionary<string, object>[] args)
         {
             foreach (var item in logDict.Values)
