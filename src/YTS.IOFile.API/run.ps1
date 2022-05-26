34635f1bba5b968862e930c372ebf11649c81dbc
YellowTulipShow <main@yellowtulipshow.site>
2022-05-26 19:23:22 +0800

    运行脚本

---
+++ src/YTS.IOFile.API/run.ps1
@@ -0,0 +1,19 @@
+$ExecutePath = $PWD
+Set-Location $PSScriptRoot
+
+Write-Output "dotnet run ./YTS.IOFile.API.csproj --urls http://127.0.0.1:16129"
+dotnet run ./YTS.IOFile.API.csproj --urls http://127.0.0.1:16129
+
+Set-Location $ExecutePath
+if ($PSScriptRoot -eq $ExecutePath) {
+    timeout.exe /T -1
+}
+
+
+# dotnet run .\YTS.IOFile.API.csproj --urls http://localhost:16129
+
+# http://localhost/index.html
+# http://localhost/ioapi/index.html
+# http://localhost:16129/index.html
+# http://localhost:16129/ioapi/index.html
+# http://localhost/ioapi/swagger/v1/swagger.json
