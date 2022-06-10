0a06ec27a025d755092528019270420322f4f591
YellowTulipShow <main@yellowtulipshow.site>
2022-06-10 11:07:05 +0800

    完善脚本逻辑

--- shell/find_all_project.ps1
+++ shell/find_all_project.ps1
@@ -2,16 +2,30 @@ $ExecutePath = $PWD
 Set-Location $PSScriptRoot
 Set-Location ..
 
+function PrintLineSplit([string]$path)
+{
+    Write-Host ""
+    Write-Host "======================================================================================="
+    Write-Host ""
+}
+
 # 查找所有项目名单
-$config_projects = "./_configs/fixed/projects.config"
-if (!(Test-Path $config_projects)) {
-    Write-Output "config file:${config_projects} not existence, find all *.csproj files"
-    New-Item -ItemType File -Force -Path $config_projects
+PrintLineSplit
+Write-Host "[Execute] Find All *.csproj Files"
+PrintLineSplit
+$cpath = "./_configs/fixed/projects.config"
+if (!(Test-Path $cpath)) {
+    Write-Host "[Config File] Not Existent: $cpath, Create ......";
+    New-Item -ItemType File -Force -Path $cpath
 }
 $list = Get-ChildItem -Recurse -Include *.csproj -Name
-$list > $config_projects
-Write-Output "Get all project file:"
-Write-Output $list
+for ($i = 0; $i -lt $list.Count; $i++) {
+    $list[$i] = $list[$i] -replace "\\","/"
+    $item = $list[$i];
+    Write-Host "project: $item"
+}
+$list > $cpath
+PrintLineSplit
 
 Set-Location $ExecutePath
 if ($PSScriptRoot -eq $ExecutePath) {
--- shell/push_nuget_packages.ps1
+++ shell/push_nuget_packages.ps1
@@ -2,11 +2,36 @@ $ExecutePath = $PWD
 Set-Location $PSScriptRoot
 Set-Location ..
 
-$nuget_source_url="https://api.nuget.org/v3/index.json"
-$config_api_key = "./_configs/nuget.org-api-key.config"
-$api = Get-Content $config_api_key
+function PrintLineSplit([string]$path)
+{
+    Write-Host ""
+    Write-Host "======================================================================================="
+    Write-Host ""
+}
 
-Set-Location "./_release/packages"
+# 查找所有项目名单
+PrintLineSplit
+Write-Host "[Execute] Push Nuget.org Packages:"
+
+$nuget_source_url="https://api.nuget.org/v3/index.json"
+$cpath = "./_configs/nuget.org-api-key.config"
+if (!(Test-Path $cpath)) {
+    Write-Host "[Config File] Not Existent: $cpath !!!";
+    PrintLineSplit
+    return
+}
+$api = Get-Content $cpath
+PrintLineSplit
+Write-Host "[Get nuget.org api-key]: $api"
+
+PrintLineSplit
+$cpath = "./_release/packages"
+if (!(Test-Path $cpath)) {
+    Write-Host "[Path Null] Not Existent: $cpath !!!";
+    PrintLineSplit
+    return
+}
+Set-Location $cpath
 $list = Get-ChildItem -Recurse -Include *.nupkg -Name
 
 if ($api) {
@@ -19,16 +44,18 @@ if ($api) {
         if (!$item) {
             continue;
         }
+
+        PrintLineSplit
+
         # 推送打包文件
         Write-Output "dotnet nuget push $item --api-key $api --source $nuget_source_url --skip-duplicate"
-        dotnet nuget push $item --api-key $api --source $nuget_source_url --skip-duplicate
-        Write-Output ""
-        Write-Output "======================================================================================="
-        Write-Output ""
+        # dotnet nuget push $item --api-key $api --source $nuget_source_url --skip-duplicate
     }
 } else {
-    Write-Output "API-KEY:$config_api_key Content is Null!!!"
+    Write-Output "API-KEY:$cpath Content is Null!!!"
 }
+
+PrintLineSplit
 
 Set-Location $ExecutePath
 if ($PSScriptRoot -eq $ExecutePath) {
