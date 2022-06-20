186bb54cd117883a85dc4b19ac3cd1720b949244
YellowTulipShow <main@yellowtulipshow.site>
2022-06-20 16:03:27 +0800

    无需代码

--- shell/release.ps1
+++ shell/release.ps1
@@ -1,19 +1,14 @@
-param([string]$runMethod)
-if ($runMethod -ne "toolScript") {
-    $ExecutePath = $PWD
-    Set-Location $PSScriptRoot
-    Set-Location ..
+$ExecutePath = $PWD
+Set-Location $PSScriptRoot
+Set-Location ..
 
-    . ./shell/global_tools.ps1
-}
+. ./shell/global_tools.ps1
 
 . ./shell/release_app.ps1 "toolScript"
 
 . ./shell/release_packages.ps1 "toolScript"
 
-if ($runMethod -ne "toolScript") {
-    Set-Location $ExecutePath
-    if ($PSScriptRoot -eq $ExecutePath) {
-        timeout.exe /T -1
-    }
+Set-Location $ExecutePath
+if ($PSScriptRoot -eq $ExecutePath) {
+    timeout.exe /T -1
 }
